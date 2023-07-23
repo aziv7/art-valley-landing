52d0e3118a62ea426542a8e0141b5eaaaa72b23f
aziv7 <57321183+aziv7@users.noreply.github.com>
2023-07-23 13:12:41 +0100

    Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,39 @@
 import React, { memo } from 'react';
-
+import img1 from '../assets/img1.webp';
+import img2 from '../assets/img2.jpg';
+import img3 from '../assets/img3.jpg';
+import img4 from '../assets/img4.jpg';
+import PictureCover from './PictureCover';
 const Hero = () => {
-  return <div>Hero</div>;
+  let items = [
+    {
+      img: img1,
+      title: 'Museum Walk',
+      subtitle: 'Museum',
+    },
+    {
+      img: img2,
+      title: 'Artist in studio',
+      subtitle: 'France - ix',
+    },
+    {
+      img: img3,
+      title: 'Beautiful place',
+      subtitle: 'xiv',
+    },
+    {
+      img: img4,
+      title: 'Nation of history',
+      subtitle: 'Renaissance art',
+    },
+  ];
+  return (
+    <div className='w-full h-screen flex'>
+      {items.map((item) => (
+        <PictureCover key={item?.title} />
+      ))}
+    </div>
+  );
 };
 
 export default memo(Hero);
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,11 +1,13 @@
 import React, { memo } from 'react';
 import Header from './Header';
+import Hero from './Hero';
 
 const Page = () => {
   return (
     <main>
       <div className='w-full h-screen overflow-hidden'>
         <Header />
+        <Hero />
       </div>
     </main>
   );
